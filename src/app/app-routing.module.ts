a7d4b8a39580dff8895e3cf1030fde8521f8c3c0
thisisnotsnorlax <kelvinsoliman5@gmail.com>
2024-10-11 12:05:11 +0800

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentlistComponent } from './components/studentlist/studentlist.component';
+import { EmployeelistComponent } from './components/employeelist/employeelist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "studentlist", component: StudentlistComponent},
+  {path: "employeelist", component: EmployeelistComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentlistComponent } from './components/studentlist/studentlist.component';
+import { EmployeelistComponent } from './components/employeelist/employeelist.component';
+import { FruitlistComponent } from './components/fruitlist/fruitlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentlistComponent,
+    EmployeelistComponent,
+    FruitlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
